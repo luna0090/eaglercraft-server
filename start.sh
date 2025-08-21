94841ef101b80e12baf62683492584e01b4c5214
luna0090 <olliej0090@outlook.com>
2025-08-21 12:36:41 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo "Starting Eaglercraft server..."
 java -Xmx512M -jar server.jar --port 25565 &
 
 echo "Starting EaglerXServer proxy on port $PORT..."
-java -Xmx256M -jar eaglerxserver.jar --server ws://localhost:25565 --port $PORT
+java -Xmx256M -jar EaglerXServer.jar --server ws://localhost:25565 --port $PORT
 
 # Keep the script running
 wait
